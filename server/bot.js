f05b811e278f08c9a82ba8eb49e5dc8d8a9d5ee3
DawnTheDurr <dawnatherton123@gmail.com>
2019-09-21 15:52:13 +0100

    More trying

--- server/bot.js
+++ server/bot.js
@@ -67,12 +67,8 @@ bot.on('message', async msg => {
 
     case 'e621-multi':
     if (msg.channel.nsfw == true){
-
-      var amount = int(args[0])
-      args.shift()
-      msg.channel.send(str(amount))
-      if (amount <= 10){
-
+      msg.channel.send(args[0])
+      if (args[0] <= 10){
         msg.channel.send('Amount is less than 10')
         var url = urlBuild(args, str(amount), 'e621')
         search(url, function(data){
