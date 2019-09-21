a0cb1c7c0f2de66d10aec902092b47e572222d52
DawnTheDurr <dawnatherton123@gmail.com>
2019-09-21 15:02:54 +0100

    Added e926 and e621-multi command, edited buildURL to work with all

--- server/bot.js
+++ server/bot.js
@@ -4,6 +4,7 @@ const request = require('request');
 const { RichEmbed } = require('discord.js');
 
 const blacklist = ['cub', 'young', 'gore', 'guro', 'death', 'snuff', 'loli', 'shota']
+const e926blacklist = ['gore', 'guro', 'death', 'snuff']
 
 bot.on('ready', () => {
   console.log('Successfully started')
@@ -24,7 +25,7 @@ bot.on('message', async msg => {
 
     if (msg.channel.nsfw == true){
 
-      var url = urlBuild(args)
+      var url = urlBuild(args, '5', 'e621')
       search(url, function(data){
         var obj = JSON.parse(data)
         var attempt = 0
@@ -61,6 +62,91 @@ bot.on('message', async msg => {
     } else {
       msg.channel.send('This command is for NSFW channels only')
     }
+
+    break;
+
+    case 'e621-multi':
+
+    if (msg.channel.nsfw == true){
+      var amount = args.shift()
+
+      if (int(amount) <= 10){
+
+        var url = urlBuild(args, amount, 'e621')
+        search(url, function(data){
+
+          var obj = JSON.parse(data)
+          var flag = false
+          var attempt = 0
+
+          for (let n = 0; n < obj.length; n++){
+            flag = false
+            for (let i = 0; i < blacklist.length; i++) {
+              if(obj[n].tags.includes(blacklist[i])){
+                flag == true
+              }
+            }
+            if (flag == false){
+              var postLink = 'https://e621.net/post/show/' + obj[n].id
+              var embed = new RichEmbed()
+              .setTitle('Link')
+              .setURL(postLink)
+              .setImage(obj[n].file_url)
+            } else {
+              attempt += 1
+            }
+          }
+
+          if (attempt >= obj.length){
+            msg.channel.send('Unable to find images')
+          }
+
+        })
+      } else {
+        msg.channel.send('Can only send 10 posts at once')
+      }
+    } else {
+      msg.channel.send('This command is for NSFW channels only')
+    }
+    break;
+
+    case 'e926':
+
+    var url = urlBuild(args, '5', 'e926')
+
+    search(url, function(data){
+      var obj = JSON.parse(data)
+      var attempt = 0
+      var post = 0
+      var flag = false
+
+      for (let n = 0; n < obj.length; n++){
+        flag = false
+        for (let i = 0; i < blacklist.length; i++) {
+          if(obj[n].tags.includes(e926blacklist[i])){
+            flag = true
+          }
+        }
+        if (flag == true){
+          attempt += 1
+        } else {
+          post = attempt
+          break
+        }
+      }
+
+      if (attempt < obj.length){
+        var postLink = 'https://e926.net/post/show/' + obj[post].id
+        var embed = new RichEmbed()
+        .setTitle('Link')
+        .setURL(postLink)
+        .setImage(obj[post].file_url)
+
+        msg.channel.send(embed)
+      } else {
+        msg.channel.send('Unable to find image')
+      }
+    })
 
     break;
   }
--- server/utils/buildURL.js
+++ server/utils/buildURL.js
@@ -1,5 +1,5 @@
-urlBuild = function(tags){
-  var url = 'https://e621.net/post/index.json?limit=5&tags=order:random+'
+urlBuild = function(tags, amount, loc){
+  var url = 'https://' + loc + '.net/post/index.json?limit=' + amount + '&tags=order:random+'
   for (x in tags){
     if (x < tags.length){
       url += tags[x] + '+';
